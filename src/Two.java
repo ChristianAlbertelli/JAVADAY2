e063dc2a64ee51dd9972dacfdd4fcd6b17c51dc2
ChristianAlbertelli <73280725+ChristianAlbertelli@users.noreply.github.com>
2020-10-27 17:46:38 +0000

    First commit

---
+++ src/Two.java
@@ -0,0 +1,15 @@
+
+public class Two extends One{
+
+	int B;
+	
+	public Two() {
+		System.out.println("Welcome to the 1st constructor of Two");
+	}
+	
+	public Two(int B) {
+		super(2);
+		System.out.println("Welcome to the 2nd constructor of Two");
+	}
+	 
+}
